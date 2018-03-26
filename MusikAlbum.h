c91f4915d95e4ad1b02971f816b4a41a753be5f2
ElitsaMilusheva <elitsa.milusheva@abv.bg>
2018-03-26 17:54:45 +0300

    Add files via upload

---
+++ MusikAlbum.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Song.h"
+
+
+class MusikAlbum
+{ private:
+    Song* songs;
+ int numberofSongs;
+ size_t capacity;
+    void copy(const MusikAlbum& other);
+    void erase();
+public:
+    MusikAlbum();
+    ~MusikAlbum();
+    MusikAlbum(Song* newSongs, int numberofSongs);
+    MusikAlbum(const MusikAlbum& other);
+
+    MusikAlbum& operator=(const MusikAlbum& other);
+
+    //getters
+    const Song* getSongs() const;
+    int getNumber() const;
+    size_t getCapacity() const;
+
+    //setters
+
+    void setSongs(const Song* newSongs, int newnumberofSongs);
+    void setCapacity(size_t newCapacity);
+
+    void addSong(const Song& other);
+    void removeSongAtIndex(size_t index);
+    double totallenght() const;
+    size_t countofSongsofGenre(const char* genre) const;
+    size_t countofSongsofSinger(const char* singer) const;
+
+}
